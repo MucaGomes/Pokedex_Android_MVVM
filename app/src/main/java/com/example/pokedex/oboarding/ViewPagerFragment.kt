3c42c57b922a278aff789b05241c1e4e1b84d783
MucaGomes <contatodevmuca@gmail.com>
2022-11-28 12:55:48 -0300

    finalizando oBoarding

---
+++ app/src/main/java/com/example/pokedex/oboarding/ViewPagerFragment.kt
@@ -0,0 +1,42 @@
+package com.example.pokedex.oboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pokedex.R
+import com.example.pokedex.databinding.FragmentHomeBinding
+import com.example.pokedex.databinding.FragmentViewPagerBinding
+import com.example.pokedex.oboarding.screens.FirstFragment
+import com.example.pokedex.oboarding.screens.SecondFragment
+
+class ViewPagerFragment : Fragment() {
+
+    private lateinit var binding: FragmentViewPagerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentViewPagerBinding.inflate(layoutInflater, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstFragment(),
+            SecondFragment()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        binding.viewPager.adapter = adapter
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/pokedex/oboarding/screens/SecondFragment.kt
@@ -0,0 +1,38 @@
+package com.example.pokedex.oboarding.screens
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
+import com.example.pokedex.R
+import com.example.pokedex.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding : FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(layoutInflater, container, false)
+
+        binding.txtFinish.setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
+            onBoardingFinished()
+        }
+        return binding.root
+    }
+
+    private fun onBoardingFinished(){
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished",true)
+        editor.apply()
+    }
+}
